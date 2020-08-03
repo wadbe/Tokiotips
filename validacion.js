8afe4d032c84b3f192637d2740963c683f842f07
wadbe <65698988+wadbe@users.noreply.github.com>
2020-08-03 15:53:55 -0300

    actualizacion

--- validacion.js
+++ validacion.js
@@ -2,21 +2,26 @@ const form = document.getElementById('formulario')
 const nombre = document.getElementById('nombre')
 const email = document.getElementById('email')
 const emailHelp= document.getElementById ('emailHelp')
-const comentario = document.getElementById('comentario');
 const nombreHelp= document.getElementById ('nombreHelp')
+const textarea= document.getElementById('comentario')
+const cometarioHelp= document.getElementById('comentarioHelp')
 
  form.addEventListener('submit', (event) => {
-     if(email.value === "" || nombre.value ==="") {
+     event.preventDefault()
+     if(email.value === "" || nombre.value ==="" || comentario.value === "") {
          if(email.value==="") {
              emailHelp.innerText= "¡Por favor, completa con tu e-mail!"
          }
          if(nombre.value==="") {
             nombreHelp.innerText= "¡Por favor, completa con tu nombre!"
         }
-        
+        if(comentario.value==="") {
+            comentarioHelp.innerText= "Este campo no puede quedar vacío."
+        }
 
      } else {
          console.log('funciona');
+    
          form.submit();
         
      }
